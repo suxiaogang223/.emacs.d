6301a235664f11a30017f25f9ebdc813f45e3f91
suxiaogang <suxiaogang223@icloud.com>
2023-08-12 17:58:45 +0800

    add key bind for eglot-format

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(tsdh-light))
  '(global-display-line-numbers-mode t)
- '(package-selected-packages '(clang-format company))
+ '(package-selected-packages '(company))
  '(scroll-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -20,6 +20,11 @@
                          ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize)
 
+;; set indent and tab-width
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(defvaralias 'c-basic-offset 'tab-width)
+
 ;; enable company-mode in some mode hooks
 (require 'company)
 (add-hook 'emacs-lisp-mode-hook 'company-mode)
@@ -28,8 +33,17 @@
 
 ;; enable lsp in some mode hooks
 (require 'eglot)
+;; set key for eglot-format
+(define-key eglot-mode-map (kbd "C-c f") 'eglot-format)
 (add-to-list 'eglot-server-programs '((c++-mode c-mode) "clangd"))
 (add-hook 'c-mode-hook 'eglot-ensure)
 (add-hook 'c++-mode-hook 'eglot-ensure)
 ;;(add-hook 'rust-mode-hook 'eglot-ensure)
 
+(require 'electric)
+(add-hook 'c-mode-hook 'electric-pair-mode)
+(add-hook 'c++-mode-hook 'electric-pair-mode)
+(add-hook 'c-mode-hook 'electric-indent-mode)
+(add-hook 'c++-mode-hook 'electric-indent-mode)
+(add-hook 'c-mode-hook 'electric-quote-mode)
+(add-hook 'c++-mode-hook 'electric-quote-mode)
