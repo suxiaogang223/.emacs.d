39e8697246bba61b4f649f62351d15e9e0bc7079
suxiaogang <suxiaogang223@icloud.com>
2023-12-16 21:16:45 +0800

    install dracula theme for emacs on fedora

--- init.el
+++ init.el
@@ -3,10 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(tsdh-light))
+ '(custom-enabled-themes '(dracula))
+ '(custom-safe-themes
+   '("603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" default))
  '(global-display-line-numbers-mode t)
- '(package-selected-packages
-   '(magit racket-mode rust-mode company))
+ '(package-selected-packages '(dracula-theme magit racket-mode rust-mode company))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
